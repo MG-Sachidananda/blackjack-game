dcfe3940e578d8cf003f894d2d159650e652433d
MG-Sachidananda <anandhoni183@gmail.com>
2021-06-16 17:35:25 +0530

    Improved Game
    Added an ADD bet button that calculates the invested amount.
    If Player wins blackjack, winnings= money invested *10.

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ let player = {
     name: "Anand",
     chips: 5000
 }
-
+count=0
 let cards = []
 let sum = 0
+let bet=0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let winningEl=document.getElementById("winnings")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
-
+let displayBet=document.getElementById("display-bet")
+let zeroBalance = document.getElementById("zeroBal")
 playerEl.textContent = player.name + ": $" + player.chips
-
+displayBet.textContent="Currently placed Bets Amount = "+ bet
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
@@ -25,6 +28,9 @@ function getRandomCard() {
         return randomNumber
     }
 }
+if(player.chips<=0){
+    zeroBalance.textContent="You dont have balance to play. All profits and losses will be void."
+}
 
 function startGame() {
     isAlive = true
@@ -33,6 +39,7 @@ function startGame() {
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
     renderGame()
+    winningEl.textContent= ""
 }
 
 function renderGame() {
@@ -47,9 +54,14 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        player.chips+= (count*1000)
+        playerEl.textContent = player.name + ": $" + player.chips
     } else {
         message = "You're out of the game!"
         isAlive = false
+        count=0
+        bet=0
+        displayBet.textContent="Currently placed Bets Amount = "+ bet
     }
     messageEl.textContent = message
 }
@@ -63,3 +75,25 @@ function newCard() {
         renderGame()        
     }
 }
+
+function addBet(){
+    if(player.chips>0){
+        bet+=100
+        displayBet.textContent="Currently placed Bets Amount = "+ bet
+        player.chips-=100
+        playerEl.textContent = player.name + ": $" + player.chips
+        count++
+    }
+}
+
+function stopGame(){
+    if(player.chips>=5000){
+        winningEl.textContent= "YOU WON " + (player.chips-5000)
+
+    }
+    else{
+        winningEl.textContent= "YOU LOSE " + (5000-player.chips)
+    }
+    
+}
+
